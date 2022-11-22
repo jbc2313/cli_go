07797f6b8bc0f7d684e8c0adf7cb8a651850584f
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-22 17:40:30 -0600

    finished example

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -22,7 +22,111 @@ var (
 	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
 )
 
-func main() {
+type item string
 
+func (i item) FilterValue () string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                               { return 1 }
+func (d itemDelegate) Spacing() int                              { return 0 }
+func (d itemDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render("> " + s)
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
+
+type model struct {
+	list     list.Model
+	choice   string
+	quitting bool
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.list.SetWidth(msg.Width)
+		return m, nil
+
+	case tea.KeyMsg:
+		switch keypress := msg.String(); keypress {
+		case "ctrl+c":
+			m.quitting = true
+			return m, tea.Quit
+
+		case "enter":
+			i, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.choice = string(i)
+			}
+			return m, tea.Quit
+		}
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	if m.choice != "" {
+		return quitTextStyle.Render(fmt.Sprintf("%s? Sounds good to me.", m.choice))
+	}
+	if m.quitting {
+		return quitTextStyle.Render("Not hungry? That’s cool.")
+	}
+	return "\n" + m.list.View()
+}
+
+func main() {
+    items := []list.Item{
+        item("lantern"),
+        item("compass"),
+        item("matches"),
+        item("axe"),
+        item("spork"),
+        item("rope"),
+        item("knife"),
+        item("backpack"),
+        item("map"),
+        item("ration"),
+        item("canteen"),
+        item("gloves"),
+        item("blanket"),
+    }
+
+    const defaultWidth = 20
+
+    l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+    l.Title = "What items will you need?"
+    l.SetShowStatusBar(true)
+    l.SetFilteringEnabled(false)
+    l.Styles.Title = titleStyle
+    l.Styles.PaginationStyle = paginationStyle
+    l.Styles.HelpStyle = helpStyle
+
+    m := model{list: l}
+
+    if _, err := tea.NewProgram(m).Run(); err != nil {
+        fmt.Println("Error running program:", err)
+        os.Exit(1)
+    }
 
 }
