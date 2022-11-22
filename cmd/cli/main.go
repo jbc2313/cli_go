6bca0d6bd120200507a025f41bbe61b4180c75df
jb corbin <101614021+jbc2313@users.noreply.github.com>
2022-11-22 17:24:55 -0600

    setting up a simple list, using bubbletea

---
+++ cmd/cli/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+
+    "fmt"
+    "os"
+    "io"
+    
+    "github.com/charmbracelet/bubbles/list"
+    tea "github.com/charmbracelet/bubbletea"
+    "github.com/charmbracelet/lipgloss"
+)
+
+const listHeight = 14
+
+var (
+    titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+    itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+func main() {
+
+
+}
